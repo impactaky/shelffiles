d3973178965b732a12e776194b8e4dec3c37fa23
Kohei Nozaki <kohei.nozaki@tx-inc.com>
2025-03-22 20:38:33 +0900

    Fix entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,17 +4,18 @@
 SCRIPT_DIR="$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd)"
 
 # Source the environment setup
-. "$SCRIPT_DIR/use_env.sh"
+. "$SCRIPT_DIR/env.sh"
 
 # Use the user's preferred shell or fall back to bash
 case "$SHELFFILES_SHELL" in
     "fish")
-        exec fish $@
+        exec fish "$@"
         ;;
     "zsh")
-        exec zsh $@
+        export ZDOTDIR="$XDG_CONFIG_HOME/zsh"
+        exec zsh "$@"
         ;;
     *)
-        exec bash $@
+        exec bash "$@"
         ;;
 esac
