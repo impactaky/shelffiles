e47cbf4f72d8b04103f18aa36e62e89e8e1880e1
Kohei Nozaki <kohei.nozaki@tx-inc.com>
2025-04-12 22:04:07 +0900

    Update example

--- example/user_env.sh
+++ example/user_env.sh
@@ -7,6 +7,3 @@ export HISTFILE="$XDG_STATE_HOME"/bash/history
 ## starship
 export STARSHIP_CONFIG="$XDG_CONFIG_HOME"/starship.toml
 export STARSHIP_CACHE="$XDG_CACHE_HOME"/starship
-
-## zsh
-export ZDOTDIR="$XDG_CONFIG_HOME/zsh"
