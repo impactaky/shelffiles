71277b79abc409f0550c513b4d9836cd01e6fd71
impactaly <impactaky@users.noreply.github.com>
2025-12-14 20:04:58 +0900

    Fixed `flake.nix` formatting. The lint should now pass.

--- flake.nix
+++ flake.nix
@@ -4,29 +4,21 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   inputs.nix-ai-tools.url = "github:numtide/nix-ai-tools";
 
-  outputs =
-    { nixpkgs, nix-ai-tools, ... }:
+  outputs = { nixpkgs, nix-ai-tools, ... }:
     let
-      systems = [
-        "aarch64-linux"
-        "x86_64-linux"
-        "aarch64-darwin"
-        "x86_64-darwin"
-      ];
+      systems =
+        [ "aarch64-linux" "x86_64-linux" "aarch64-darwin" "x86_64-darwin" ];
 
       # Load user packages or use default packages if user packages don't exist
-      loadPackages =
-        _: pkgs: nix-ai-tools:
-        let
-          packagesPath = ./packages.nix;
-        in
-        import packagesPath pkgs nix-ai-tools;
-    in
-    {
+      loadPackages = _: pkgs: nix-ai-tools:
+        let packagesPath = ./packages.nix;
+        in import packagesPath pkgs nix-ai-tools;
+    in {
       packages = nixpkgs.lib.genAttrs systems (system: {
         default = nixpkgs.legacyPackages.${system}.buildEnv {
           name = "shelffiles-env";
-          paths = loadPackages system nixpkgs.legacyPackages.${system} (nix-ai-tools.packages.${system} or { });
+          paths = loadPackages system nixpkgs.legacyPackages.${system}
+            (nix-ai-tools.packages.${system} or { });
         };
       });
     };
